d6a609899a2ffbcaf00615a49301e7fd7e486177
Ryu Tempest <92064565+ryutempesto@users.noreply.github.com>
2023-03-28 21:48:40 -0400

    Actualizaciones

--- Controllers/Perfil.controller.js
+++ Controllers/Perfil.controller.js
@@ -3,7 +3,7 @@ const conexion = require("../Database/database"),bcryptjs = require("bcryptjs"),
 class Profile {
     userProfile(req,res){
                const id = req.session.id_user;
-        const generalQuery = "SELECT * FROM `usuario` INNER JOIN rol ON usuario.rol_id = rol.id_rol INNER JOIN estatus ON usuario.estatus_id = estatus.id_status INNER JOIN pais ON usuario.pais_id = id_pais INNER JOIN wallet ON wallet.user_id = usuario.id WHERE usuario.id = ?";
+        const generalQuery = "SELECT * FROM usuario INNER JOIN rol ON usuario.rol_id = rol.id_rol  JOIN estatus ON usuario.estatus_id = estatus.id_status  JOIN pais ON usuario.pais_id = pais.id_pais JOIN wallet ON usuario.id = wallet.id_wallet WHERE usuario.id = ?";
         conexion.query(generalQuery,[id],(err,results)=>{
             if(err){
                 console.error(err)
@@ -16,6 +16,8 @@ class Profile {
         })
       
     }
+
+
     resetPasswordAdmin(req,res){
         const {id,old_password,password,repeatPassword} = req.body;
         console.log(id,old_password,password,repeatPassword);
@@ -23,9 +25,8 @@ class Profile {
         const updateQuery = "UPDATE usuario SET password = ?";
         // Verificando Que las contraseña sean iguales.
         conexion.query(query,[id],async (error, results) =>{
-            console.log(results)
-            if(results = []){
-                return res.status(404).render("layouts/userPorfile",{
+            if(results.length == 0){
+                return await res.render("layouts/userPorfile",{
                     alert: true,
                     alertIcon: 'error',
                     alertTitle: "Datos no encontrados",
@@ -37,7 +38,7 @@ class Profile {
             }else{
                 const verifyPassword = await bcryptjs.compare( old_password, results[0].password);
                 if(verifyPassword === false){
-                    return res.status(404).render("layouts/userPorfile",{
+                    return await res.render("layouts/userPorfile",{
                         alert: true,
                         alertIcon: 'error',
                         alertTitle: "Contraseña actual no conincide",
@@ -50,7 +51,7 @@ class Profile {
                 const passwordHaash = await bcryptjs.hash(password, 8);
 
                 if(password != repeatPassword){
-                    return res.status(404).render("layouts/userPorfile",{
+                    return await res.render("layouts/userPorfile",{
                         alert: true,
                         alertIcon: 'error',
                         alertTitle: "Contraseña Nueva no conincide",
@@ -60,9 +61,9 @@ class Profile {
                         results:results
                     })
                 }
-                conexion.query(updateQuery, [passwordHaash], err =>{
+                conexion.query(updateQuery, [passwordHaash],async err =>{
                     if(err){
-                        return res.status(404).render("layouts/userPorfile",{
+                        return await res.render("layouts/userPorfile",{
                             alert: true,
                             alertIcon: 'error',
                             alertTitle: "Error al actualizar",
@@ -72,7 +73,7 @@ class Profile {
                             results:results
                         })
                     }else{
-                        return res.status(200).render("layouts/userPorfile",{
+                        return await res.render("layouts/userPorfile",{
                             alert: true,
                             alertIcon: 'success',
                             alertTitle: "Contraseña actualizada",
@@ -88,6 +89,11 @@ class Profile {
             }
         });
     }
+
+
+
+
+
     resetPassword(req,res){
         const {id,old_password,password,repeatPassword} = req.body;
         const tipo_solicitud = 0;
