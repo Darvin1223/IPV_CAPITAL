7f2541a4f83482b0d472eac5235d8c172e9c3f0a
Ryu Tempest <92064565+ryutempesto@users.noreply.github.com>
2023-04-10 13:51:30 -0400

    .

--- Controllers/Time.controllers.js
+++ Controllers/Time.controllers.js
@@ -22,16 +22,34 @@ class TimeController{
             await mysql2.ejecutar_query_con_array(`UPDATE usuario SET estatus_id = 6 WHERE id = ?`,[usuarios.id_usuario])
         });
 
+        console.log('Actualizando Usuarios Inactivos')
 
     }
+
+    async Actualizar_Planes_Activos(){
+
+        let planes_activos = await mysql2.ejecutar_query_con_array(`SELECT * FROM planes_activos WHERE NOW() > fecha_expiracion AND disponible = 0`,[]);
+
+        if(planes_activos.length == 0) return 0;
+
+        await mysql2.ejecutar_query_con_array(`UPDATE planes_activos SET disponible = ? WHERE NOW() > fecha_expiracion AND disponible = 0`,[1]);
+
+        console.log('Actualizando Planes');
+
+    }
+
+
 }
 
 //Controlador
 let main = new TimeController()
 
-//Establecer Tiempo
-setInterval(main.UsuariosInactivos,obtener_tiempo(5));
 
+var cinco_minutos = obtener_tiempo(5);
+
+//Establecer Tiempo
+setInterval(main.UsuariosInactivos,cinco_minutos);
+setInterval(main.Actualizar_Planes_Activos,5*60000);
 
 module.exports = new TimeController()
 
